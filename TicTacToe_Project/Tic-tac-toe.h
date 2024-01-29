fc545640b02089537804aeb53c556b9b94dfeef2
reigletheaters <155500420+reigletheaters@users.noreply.github.com>
2024-01-28 20:05:57 -0800

    Update Tic-tac-toe.h

--- TicTacToe_Project/Tic-tac-toe.h
+++ TicTacToe_Project/Tic-tac-toe.h
@@ -1,1 +1,30 @@
 
+#ifndef TICTACTOE_H
+#define TICTACTOE_H
+
+class TicTacToeGame {
+public:
+  // Constructor and Destructor
+  TicTacToeGame();
+  ~TicTacToeGame();
+
+  // Public function to play the game
+  void play();
+
+private:
+  char **pp_cGrid; // Pointer to a pointer to character
+
+  void initializeGrid();
+  void displayGrid();
+  bool isMoveValid(int row, int col);
+  bool isGridFull();
+  bool checkWin(char player);
+
+  // AI function for computer's move
+  void computerMove();
+
+  // Function to deallocate memory for the grid
+  void deallocateGrid();
+};
+
+#endif // TICTACTOE_H
