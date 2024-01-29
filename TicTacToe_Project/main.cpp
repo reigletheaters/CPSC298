2373bd17d395c90a73c08341a86332c7a899bf7d
reigletheaters <155500420+reigletheaters@users.noreply.github.com>
2024-01-28 20:05:20 -0800

    Update main.cpp

--- TicTacToe_Project/main.cpp
+++ TicTacToe_Project/main.cpp
@@ -1,1 +1,15 @@
+#include "Tictactoe.h"
+
+int main() {
+    // Create an instance of game
+    TicTacToeGame* p_game = new TicTacToeGame();
+
+
+    p_game->play();
+
+
+    delete p_game;
+
+    return 0;
+}
 
